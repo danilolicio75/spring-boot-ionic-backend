47b24f30cb7c86e5b918bfb7358c1086c56df3c8
Danilo Licio Dias Barbosa <danilolicio75@gmail.com>
2021-01-20 17:52:00 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/licio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/licio/cursomc/resources/CategoriaResource.java
@@ -36,7 +36,6 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
@@ -44,4 +43,9 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/licio/cursomc/services/CategoriaService.java
+++ src/main/java/com/licio/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.licio.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.licio.cursomc.domain.Categoria;
 import com.licio.cursomc.repositories.CategoriaRepository;
+import com.licio.cursomc.services.exception.DataIntegratyException;
 import com.licio.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegratyException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
